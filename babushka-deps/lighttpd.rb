cf790476bab7545178f7f396cba515f3497824e3
Ryan Doyle <ryan@doylenet.net>
2013-06-27 20:07:52 +1000

    Docdoot needs to exist before lighttpd is started/restart

--- babushka-deps/lighttpd.rb
+++ babushka-deps/lighttpd.rb
@@ -11,6 +11,7 @@ dep 'lighttpd.conf is configured' do
   met? { Babushka::Renderable.new('/etc/lighttpd/lighttpd.conf').from?(dependency.load_path.parent / "erb/lighttpd.conf.erb") }
   meet { render_erb "erb/lighttpd.conf.erb", :to => '/etc/lighttpd/lighttpd.conf' }
   after {
+    make_sure_there_is_a_docroot
     log "restarting lighttpd..."
     shell "/etc/init.d/lighttpd restart"
   }
@@ -19,7 +20,11 @@ end
 dep 'lighttpd is started' do
   requires 'lighttpd.bin'
   # Allows lighttpd to start even if the rpishell site isn't deployed
-  before { '/var/www/rpishell'.p.mkdir if !'/var/www/rpishell'.p.exist? }
+  before { make_sure_there_is_a_docroot }
   met? { shell "/etc/init.d/lighttpd status" }
   meet { shell "/etc/init.d/lighttpd start" }
+end
+
+def make_sure_there_is_a_docroot
+  '/var/www/rpishell'.p.mkdir if !'/var/www/rpishell'.p.exist?
 end
